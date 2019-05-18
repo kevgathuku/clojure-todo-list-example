0b779c595ff9dfa04f8b58ffcf40c97770a8c025
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-18 13:29:49 +0300

    Install ring library

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/kevgathuku/clojure-todo-list-example"
   :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
             :url "https://creativecommons.org"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring "1.7.1"]]
   :main todo-list.core
   :repl-options {:init-ns todo-list.core})
