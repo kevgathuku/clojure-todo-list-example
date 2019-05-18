b0de3c914709f9db008fca5adcaf1166d7511ab4
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-18 13:19:10 +0300

    Initialize todo list project
    - Fix initial failing tests
    - Add project details to project.clj/README

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject todo-list "0.1.0-SNAPSHOT"
+  :description "A Todo List server-side webapp using Ring & Compojure"
+  :url "https://github.com/kevgathuku/clojure-todo-list-example"
+  :license {:name "Creative Commons Attribution Share-Alike 4.0 International"
+            :url "https://creativecommons.org"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns todo-list.core})
---
+++ test/todo_list/core_test.clj
@@ -0,0 +1,7 @@
+(ns todo-list.core-test
+  (:require [clojure.test :refer :all]
+            [todo-list.core :refer :all]))
+
+(deftest a-test
+  (testing "Basic Test"
+    (is (= 1 1))))
