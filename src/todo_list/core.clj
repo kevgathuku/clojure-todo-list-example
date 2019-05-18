378b8f084e0b99aa4e5138b1b34e34dfd8322f55
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-18 14:51:06 +0300

    Add 404 response for requests to non-existent pages

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -6,12 +6,18 @@
   "A ring handler to process all requests sent to the webapp.
   Takes the request map as its argument and returns a response map"
   [request]
-  {:status 200
-   :body
-     "<h1>Hello, Clojure World</h1>
-     <p>Welcome to your first Clojure app.
-       This message is returned regardless of the route, sorry<p>"
-   :headers {"Content-Type" "text/html"}})
+  (if (= "/" (:uri request))
+    {:status 200
+     :body
+       "<h1>Hello, Clojure World</h1>
+       <p>Welcome to your first Clojure app.<p>"
+     :headers {"Content-Type" "text/html"}}
+    
+    {:status 404
+     :body
+       "<h1>This is not the page you are looking for</h1>
+       <p>Sorry, the page you requested was not found!</p>"
+     :headers {"Content-Type" "text/html"}}))
 
 (defn -main
   "A simple web server using Ring & Jetty"
