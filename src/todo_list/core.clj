70ddbb791df3a71da3474a0d81fa137d08aa1f09
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-18 15:56:34 +0300

    Add -dev-main function with reload functionality

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,5 +1,6 @@
 (ns todo-list.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 
 (defn welcome
@@ -9,8 +10,8 @@
   (if (= "/" (:uri request))
     {:status 200
      :body
-       "<h1>Hello, Clojure World</h1>
-       <p>Welcome to your first Clojure app.<p>"
+       "<h1>Hello, from Clojure!</h1>
+       <p>Welcome to your first Clojure app, I now update automatically.<p>"
      :headers {"Content-Type" "text/html"}}
     
     {:status 404
@@ -18,6 +19,13 @@
        "<h1>This is not the page you are looking for</h1>
        <p>Sorry, the page you requested was not found!</p>"
      :headers {"Content-Type" "text/html"}}))
+
+(defn -dev-main
+  "A simple web server using Ring & Jetty that reloads code changes via the development profile of Leiningen"
+  [port-number]
+  (jetty/run-jetty
+    (wrap-reload #'welcome)
+    {:port (Integer. port-number)}))
 
 (defn -main
   "A simple web server using Ring & Jetty"
