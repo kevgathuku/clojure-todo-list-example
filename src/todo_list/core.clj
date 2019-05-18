a936eaeb5a6132ee55f29b64f37cc76198047b9c
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-18 14:47:48 +0300

    Extract request handler into a separate function

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,12 +1,21 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]))
 
+
+(defn welcome
+  "A ring handler to process all requests sent to the webapp.
+  Takes the request map as its argument and returns a response map"
+  [request]
+  {:status 200
+   :body
+     "<h1>Hello, Clojure World</h1>
+     <p>Welcome to your first Clojure app.
+       This message is returned regardless of the route, sorry<p>"
+   :headers {"Content-Type" "text/html"}})
+
 (defn -main
   "A simple web server using Ring & Jetty"
   [port-number]
   (jetty/run-jetty
-    (fn [request]
-      {:status 200
-       :body "<h1>Hello, Clojure </h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the route, sorry</p>"
-       :headers {"Content-Type" "text/html"}})
+    welcome
     {:port (Integer. port-number)}))
