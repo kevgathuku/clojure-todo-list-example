c5b99091cb5c57b3a7ff3922fe8c744e7ca7a6cb
Kevin Ndung'u <kevgathuku@gmail.com>
2019-05-18 13:43:50 +0300

    Add basic web server using jetty
    - Accept a port as an argument to the main function
    - The port can be provided on the cmd line e.g. lein run 3000
    - Use java's Integer. to convert the port-number argument to an integer

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,7 +1,12 @@
 (ns todo-list.core
-  (:require [clojure.string :as string]))
+  (:require [ring.adapter.jetty :as jetty]))
 
 (defn -main
-  "I don't do a whole lot."
-  []
-  (println (string/reverse "RedRum")))
+  "A simple web server using Ring & Jetty"
+  [port-number]
+  (jetty/run-jetty
+    (fn [request]
+      {:status 200
+       :body "<h1>Hello, Clojure </h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the route, sorry</p>"
+       :headers {"Content-Type" "text/html"}})
+    {:port (Integer. port-number)}))
